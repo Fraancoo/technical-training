e067775f97137265feb5ba80f4fdc7e9b4304358
Fraancoo <fernandofc2000@gmail.com>
2024-09-09 12:02:41 -0700

    Actualización del valor default del campo date_availability del modelo EstateProperty

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -25,7 +25,7 @@ class EstateProperty(models.Model):
     name = fields.Char(string="Nombre", required=True)
     description = fields.Text(string="Descripción")
     postcode = fields.Char(string="Código postal")
-    date_availability = fields.Date(string="Disponibilidad", copy=True, default=lambda self: self._default_date_availability())
+    date_availability = fields.Date(string="Disponibilidad", copy=True, default=fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float(string="Precio deseado", required=True)
     selling_price = fields.Float(string="Precio de venta", readonly=True, copy=True)
     bedrooms = fields.Integer(string="Cuartos", default=2)
@@ -40,7 +40,3 @@ class EstateProperty(models.Model):
     )
     active = fields.Boolean(string="Activo", default=True)
     state = fields.Selection(string="Estado", selection=STATE_OPTS, required=True, copy=False, default='new')
-
-    @api.model
-    def _default_date_availability(self):
-        return fields.Date.today() + timedelta(days=90)
