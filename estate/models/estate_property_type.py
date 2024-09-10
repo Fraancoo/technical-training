71a99934a7c89719272ca29aae9272efab72eb51
Fraancoo <fernandofc2000@gmail.com>
2024-09-10 16:37:20 -0700

    Stat button

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class EstatePropertyType(models.Model):
@@ -10,7 +10,14 @@ class EstatePropertyType(models.Model):
     active = fields.Boolean(string='Activo', default=True)
     sequence = fields.Integer(string='Orden', default=10)
     property_ids = fields.One2many(string="Propiedades", comodel_name="estate.property", inverse_name="property_type_id")
+    offer_ids = fields.One2many(string='Ofertas', comodel_name='estate.property.offer', inverse_name='property_type_id')
+    offer_count = fields.Integer(string='# de ofertas', compute='_compute_offer_count')
 
     _sql_constraints = [
         ("name_unique", "unique(name)", "Ya existe un tipo de propiedad con ese nombre"),
     ]
+
+    @api.depends('offer_ids')
+    def _compute_offer_count(self):
+        for record in self:
+            record.offer_count = len(record.offer_ids)
