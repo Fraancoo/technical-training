9c9a16b36724d9fed79218e587f20ce6005821c5
Fraancoo <fernandofc2000@gmail.com>
2024-09-10 08:00:04 -0700

    Se movieron los campos validity y date_deadline del modelo EstateProperty al modelo EstatePropertyOffer

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 STATE_OPTS = [
@@ -15,3 +15,15 @@ class EstatePropertyOffer(models.Model):
     state = fields.Selection(string='Estatus', selection=STATE_OPTS, copy=False)
     partner_id = fields.Many2one(string='Partner', comodel_name='res.partner', required=True)
     property_id = fields.Many2one(string='Propiedad', comodel_name='estate.property', required=True)
+    validity = fields.Integer(string='Validación', required=True, default=7)
+    date_deadline = fields.Integer(string='Fecha límite', compute='_compute_date_deadline', inverse='_inverse_date_deadline')
+
+    @api.depends('validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            record.date_deadline = fields.Date.add(fields.Date.today(), days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            resp_date = record.date_deadline - fields.Date.today()
+            record.validity = resp_date.days
