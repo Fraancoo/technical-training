43913ec62a38fd04f48bcdabe7d2ad1b58aea207
Fraancoo <fernandofc2000@gmail.com>
2024-09-10 12:22:37 -0700

    Se regresó el campo sequence porque no sé leer

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -4,10 +4,11 @@ from odoo import models, fields
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Modelo para ver los tipos de propiedades"
-    _order = "name"
+    _order = "sequence"
     
     name = fields.Char(string="Nombre", required=True)
     active = fields.Boolean(string='Activo', default=True)
+    sequence = fields.Integer(string='Orden', default=10)
     property_ids = fields.One2many(string="Propiedades", comodel_name="estate.property", inverse_name="property_type_id")
 
     _sql_constraints = [
