be2e7df892d9ff9cc1820dd604dc29f02d7c5282
Fraancoo <fernandofc2000@gmail.com>
2024-09-10 10:43:40 -0700

    Se comentó el sql_constraint del campo precio del modelo EstatePropertyOffer para hacer pruebas

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -19,9 +19,9 @@ class EstatePropertyOffer(models.Model):
     validity = fields.Integer(string='Validación', required=True, default=7)
     date_deadline = fields.Date(string='Fecha límite', compute='_compute_date_deadline', inverse='_inverse_date_deadline')
 
-    _sql_constraints = [
-        ("price_check", "check(price > 0)", "El precio debe ser mayor a 0"),
-    ]
+    # _sql_constraints = [
+    #     ("price_check", "check(price > 0)", "El precio debe ser mayor a 0"),
+    # ]
 
     @api.depends('validity')
     def _compute_date_deadline(self):
